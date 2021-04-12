0d41b4015a45bbcfb41591e3509d2630ae76e3c1
CC1618 <>
2021-04-12 13:13:09 +0200

    pde Heston pricing debug2

--- Charles/MonteCarlo.py
+++ Charles/MonteCarlo.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.stats import norm
+import matplotlib.pyplot as plt
 
 class MonteCarloPricing():
     def __init__(self, N, p_conf=.95, M=252):
@@ -9,28 +10,24 @@ class MonteCarloPricing():
 
     def gen_BS_prices(self, s0, r, σ, T, t=0, **kwargs):
         dt, nd =(T - t) / self.M, (T - t) * self.M
-        w = np.zeros(nd)
         s = np.ones(nd) * s0
         for i in range(1, nd):
-            w[i] = w[i-1] + np.random.normal() * np.sqrt(dt)
-            s[i] = s0 * np.exp((r - σ**2 / 2) * dt + σ * w[i])
+            N = np.random.normal()
+            s[i] = s[i-1] * ( 1 + r * dt + σ * np.sqrt(dt) * N)
         return s
 
     def gen_Hn_prices(self, s0, r, v0, κ, θ, γ, ρ, T, t=0, **kwargs):
         dt, nd =(T - t) / self.M, (T - t) * self.M
-        avg  = np.array([0, 0])
-        cov = np.matrix([[1, ρ], [ρ, 1]])
-        w   = np.random.multivariate_normal(avg, cov, nd)
-        w_s = w[:,0]*np.sqrt(dt)
-        w_v = w[:,1]*np.sqrt(dt)
-
         v = np.zeros(nd)
         v[0] = v0
         s = np.zeros(nd)
         s[0] = s0
         for t in range(1,nd):
-            v[t] = np.abs(v[t-1] + κ*(θ-v[t-1])*dt + γ*np.sqrt(v[t-1])*w_v[t])
-            s[t] = s[t-1]*np.exp((r - v[t]/2)*dt + np.sqrt(v[t])*w_s[t])
+            Nv = np.random.normal()
+            Nx = np.random.normal()
+            Ns = ρ * Nv + np.sqrt(1 - ρ**2) * Nx
+            v[t] = v[t-1] + κ * (θ - v[t-1]) * dt + γ*np.sqrt(v[t-1]*dt)*Nv
+            s[t] = s[t-1] * (1 + r * dt + np.sqrt(v[t-1] * dt) * Ns)
         return s
 
     def get_payoff_euro_call(self, s, K, **kwargs):
@@ -64,28 +61,28 @@ class MonteCarloPricing():
 
 if __name__ == '__main__':
     ########### Monte Carlo ###########
-    N = 1e4         # Number of sample paths
+    N = 1e3         # Number of sample paths
 
     ########### Price Dynamics ###########
     # Black and Scholes Parameters:
     T = 1         # Maturity in years
     x = 100       # Initial price
-    r = .03       # Drift / Risk free return
+    r = 0       # Drift / Risk free return
     σ = .2        # Volatility
 
     # Heston Parameters:
-    T = 1         # Maturity in years
+    T  = 1         # Maturity in years
     s0 = 100      # Initial price
-    r  = .01      # Drift / Risk free return
-    v0 = .05      # Initial Variance
-    κ  = .3       # Mean reversion factor of the variance
-    θ  = .1       # Mean of the variance
-    γ  = .15      # Volatility of the variance
-    ρ  = -.2      # Leverage factor
+    r  = 0      # Drift / Risk free return
+    v0 = .04     # Initial Variance
+    κ  = 0       # Mean reversion factor of the variance
+    θ  = .04       # Mean of the variance
+    γ  = 0      # Volatility of the variance
+    ρ  = -.7      # Leverage factor
 
     ########### Option Types ###########
     # European Call Parameters:
-    K = 110       # Stike price
+    K = 100       # Stike price
 
     # For Call Parameters:
     τ = 6 * 21   # Start date
@@ -94,28 +91,28 @@ if __name__ == '__main__':
     mc = MonteCarloPricing(N)
 
     # European Call
-    #print(' ------- European Call -------')
-    #payoff_fct = mc.get_payoff_euro_call
-
-    ## Black & Scholes
-    #gen_fct = mc.gen_BS_prices
-    #x = mc.price(gen_fct, payoff_fct, s0=s0, r=r, σ=σ, T=T, K=K)
-    #print('\t Black & Scholes: ', x)
-
-    ## Heston
-    #gen_fct = mc.gen_Hn_prices
-    #x = mc.price(gen_fct, payoff_fct, s0=s0, r=r, v0=v0, κ=κ, θ=θ, γ=γ, ρ=ρ, T=T, K=K)
-    #print('\t Heston: ', x)
-
-
-    # Forward Call
-    print('\n ------- Forward Call -------')
-    payoff_fct = mc.get_payoff_fwrd_call
+    print(' ------- European Call -------')
+    payoff_fct = mc.get_payoff_euro_call
 
     ## Black & Scholes
     gen_fct = mc.gen_BS_prices
-    x = mc.price(gen_fct, payoff_fct, s0=s0, r=r, σ=σ, T=T, τ=τ, pK=pK)
+    x = mc.price(gen_fct, payoff_fct, s0=s0, r=r, σ=σ, T=T, K=K)
     print('\t Black & Scholes: ', x)
+
+    ## Heston
+    gen_fct = mc.gen_Hn_prices
+    x = mc.price(gen_fct, payoff_fct, s0=s0, r=r, v0=v0, κ=κ, θ=θ, γ=γ, ρ=ρ, T=T, K=K)
+    print('\t Heston: ', x)
+
+
+    # Forward Call
+    #print('\n ------- Forward Call -------')
+    #payoff_fct = mc.get_payoff_fwrd_call
+
+    ## Black & Scholes
+    #gen_fct = mc.gen_BS_prices
+    #x = mc.price(gen_fct, payoff_fct, s0=s0, r=r, σ=σ, T=T, τ=τ, pK=pK)
+    #print('\t Black & Scholes: ', x)
 
     ## Heston
     #gen_fct = mc.gen_Hn_prices
--- Charles/pde_Hn_call.py
+++ Charles/pde_Hn_call.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from copy import deepcopy
 
 class PDEHnPricing():
     def __init__(self, N, I, J, scheme):
@@ -19,13 +19,14 @@ class PDEHnPricing():
                 d = Δt / Δs * (r * s[i] + s[i]**2 * v[j] / 2 / Δs)
                 e = Δt / Δv * (α + σ**2 * v[j] / 2 / Δv)
                 f = 1 - 2 * (b + c) - Δt * (r * (1 + s[i] / Δs) + α / Δv)
-                M[i, j] = np.array([[a, b, a], [c, f, e], [a, d, a]])
+                M[i, j] = np.array([[a, b, -a], [c, f, e], [-a, d, a]])
         return M
 
-    def set_call_boundaries(self, u_n, s, z, K, r):
+    def set_call_boundaries(self, u_n, s, z, K, r, Δs):
         u_n[0,:]  = np.zeros(self.J)
-        u_n[-1,:] = np.ones(self.J) * np.maximum(s[-1] - K, 0) * np.exp(-r * z)
-        u_n[:,0]  = np.maximum(s - K, 0) * np.exp(-r * z)
+        u_n[-1,:] = np.ones(self.J) * np.maximum(s[-1] - K * np.exp(-r * z), 0)
+        u_n[-2,:] = u_n[-1,:] - Δs
+        u_n[:,0]  = np.maximum(s - K * np.exp(-r * z), 0)
         u_n[:,-1] = s
         return u_n
 
@@ -37,6 +38,7 @@ class PDEHnPricing():
         #s_max = K * np.exp(8 * np.sqrt(v0) * np.sqrt(T))
         v_min = 0
         v_max = v0 * np.exp(8 * σ * np.sqrt(T))
+        v_max = 2 * v0 
         τ = np.linspace(t_min, t_max, self.N)
         s = np.linspace(s_min, s_max, self.I)
         v = np.linspace(v_min, v_max, self.J)
@@ -52,29 +54,33 @@ class PDEHnPricing():
             else:
                 u_n = np.zeros((self.I, self.J))
                 z = n * Δt
-                u_n = self.set_call_boundaries(u_n, s, z, K, r)
-                for i in range(1, self.I - 1):
+                u_n = self.set_call_boundaries(u_n, s, z, K, r, Δs)
+                for i in range(1, self.I - 2):
                     for j in range(1, self.J - 1):
                         u_n[i, j] = np.sum(M[i, j] * u_N[i-1:i+2, j-1:j+2])
-                u_N = u_n
-        i = min([x for x in range(len(s)) if s[x]>=s0])
-        j = min([x for x in range(len(s)) if v[x]>=v0])
-        return u_N[i, j] * s0/s[i]
+                u_N = deepcopy(u_n)
+        #print(u_N)
+        i_u = min([x for x in range(len(s)) if s[x]>=s0])
+        i_d = max([x for x in range(len(s)) if s[x]<=s0])
+        j_u = min([x for x in range(len(v)) if v[x]>=v0])
+        j_d = max([x for x in range(len(v)) if v[x]<=v0])
+        u0 = np.mean([u_N[i_u, j_u],u_N[i_u, j_d],u_N[i_d, j_u],u_N[i_d, j_d]])
+        return u0
 
 
 if __name__ == '__main__':
     scheme = 'explicit'
-    N  = 20
-    I  = 20
-    J  = 20
+    N  = 1000
+    I  = 25
+    J  = 24
     s0 = 100
-    K  = 110
+    K  = 100
     T  = 1
     v0 = .04
-    r  = .03
-    σ  = .2
+    r  = 0
+    σ  = 0
     ρ  =-.7
-    κ  = .8
+    κ  = 0
     θ  = .04
     λ  = 0
 
